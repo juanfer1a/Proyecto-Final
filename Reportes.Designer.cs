835c912a205a739488c3917d7bec5fcc81fe39cb
juanfer1a <jundi1a@gmail.com>
2021-05-27 20:15:52 -0500

    cambio panel reporte ventas

--- Reportes.Designer.cs
+++ Reportes.Designer.cs
@@ -37,7 +37,6 @@
             this.btnImprimir = new System.Windows.Forms.Button();
             this.pnlVentas = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.btnCerrarVenta = new System.Windows.Forms.Button();
             this.btnGenerar = new System.Windows.Forms.Button();
             this.pnlMovimientos = new System.Windows.Forms.Panel();
@@ -50,6 +49,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnCerrarBalance = new System.Windows.Forms.Button();
             this.btnGenerarBalance = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.lbLDesde = new System.Windows.Forms.Label();
+            this.lblHasta = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.pnlVentas.SuspendLayout();
             this.pnlMovimientos.SuspendLayout();
@@ -115,13 +118,16 @@
             // 
             // pnlVentas
             // 
+            this.pnlVentas.Controls.Add(this.lblHasta);
+            this.pnlVentas.Controls.Add(this.lbLDesde);
+            this.pnlVentas.Controls.Add(this.dateTimePicker2);
+            this.pnlVentas.Controls.Add(this.dateTimePicker1);
             this.pnlVentas.Controls.Add(this.label1);
-            this.pnlVentas.Controls.Add(this.comboBox1);
             this.pnlVentas.Controls.Add(this.btnCerrarVenta);
             this.pnlVentas.Controls.Add(this.btnGenerar);
-            this.pnlVentas.Location = new System.Drawing.Point(304, 92);
+            this.pnlVentas.Location = new System.Drawing.Point(122, 26);
             this.pnlVentas.Name = "pnlVentas";
-            this.pnlVentas.Size = new System.Drawing.Size(200, 100);
+            this.pnlVentas.Size = new System.Drawing.Size(396, 166);
             this.pnlVentas.TabIndex = 8;
             this.pnlVentas.Visible = false;
             // 
@@ -135,22 +141,11 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Tipo Reporte";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Categoria",
-            "Producto"});
-            this.comboBox1.Location = new System.Drawing.Point(18, 33);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(164, 21);
-            this.comboBox1.TabIndex = 2;
-            // 
             // btnCerrarVenta
             // 
-            this.btnCerrarVenta.Location = new System.Drawing.Point(133, 65);
+            this.btnCerrarVenta.Location = new System.Drawing.Point(272, 118);
             this.btnCerrarVenta.Name = "btnCerrarVenta";
-            this.btnCerrarVenta.Size = new System.Drawing.Size(49, 28);
+            this.btnCerrarVenta.Size = new System.Drawing.Size(64, 28);
             this.btnCerrarVenta.TabIndex = 1;
             this.btnCerrarVenta.Text = "Cerrar";
             this.btnCerrarVenta.UseVisualStyleBackColor = true;
@@ -158,7 +153,7 @@
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Location = new System.Drawing.Point(18, 65);
+            this.btnGenerar.Location = new System.Drawing.Point(272, 57);
             this.btnGenerar.Name = "btnGenerar";
             this.btnGenerar.Size = new System.Drawing.Size(64, 28);
             this.btnGenerar.TabIndex = 0;
@@ -268,6 +263,42 @@
             this.btnGenerarBalance.Text = "Generar";
             this.btnGenerarBalance.UseVisualStyleBackColor = true;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(25, 65);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 5, 27, 20, 6, 49, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(25, 127);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker2.TabIndex = 5;
+            this.dateTimePicker2.Value = new System.DateTime(2021, 5, 27, 20, 6, 49, 0);
+            // 
+            // lbLDesde
+            // 
+            this.lbLDesde.AutoSize = true;
+            this.lbLDesde.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLDesde.Location = new System.Drawing.Point(22, 43);
+            this.lbLDesde.Name = "lbLDesde";
+            this.lbLDesde.Size = new System.Drawing.Size(48, 15);
+            this.lbLDesde.TabIndex = 6;
+            this.lbLDesde.Text = "Desde";
+            // 
+            // lblHasta
+            // 
+            this.lblHasta.AutoSize = true;
+            this.lblHasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHasta.Location = new System.Drawing.Point(22, 104);
+            this.lblHasta.Name = "lblHasta";
+            this.lblHasta.Size = new System.Drawing.Size(44, 15);
+            this.lblHasta.TabIndex = 7;
+            this.lblHasta.Text = "Hasta";
+            // 
             // frm_Reportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -308,7 +339,6 @@
         private System.Windows.Forms.Button btnCerrarVenta;
         private System.Windows.Forms.Button btnGenerar;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel pnlMovimientos;
         private System.Windows.Forms.Label label2;
@@ -320,5 +350,9 @@
         private System.Windows.Forms.Button btnCerrarBalance;
         private System.Windows.Forms.Button btnGenerarBalance;
         private System.Windows.Forms.DateTimePicker datBalance;
+        private System.Windows.Forms.Label lblHasta;
+        private System.Windows.Forms.Label lbLDesde;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
